854bb4872f1eec870ee6bff36ea3c6560c2af81b
Daiane Farias <daiaanebarbosaf@gmail.com>
2024-04-18 14:29:29 -0300

    criando conteúdos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+
+import { HomeContainer } from "./styles";
+
 
  export function App() {
-
   return (
-    <>
-    <h1>Hello</h1>
-    </>
+        
+    <HomeContainer>          
+      <a href="https://www.instagram.com/anhanguera.iguatemi" target="_blank">
+        
+      </a>
+
+      <h1>Anhanguera Salvador Iguatemi</h1>
+      <p className="username">1ª jornada  de conscientização sobre TEA</p>
+
+      <img className="avatar" src="./assets/anhanguera.png"/>
+      <ul>
+      <li>
+          <a 
+            href="https://doity.com.br/anhangueraoficinatea03" target="_blank">17 de Abril - Oficina 03
+          </a>
+        </li>
+        <li>
+          <a 
+            href="https://doity.com.br/anhangueraoficinatea04" target="_blank">18 de Abril - Oficina 03
+          </a>
+        </li>
+      </ul>
+    </HomeContainer>
+
   )
 }
