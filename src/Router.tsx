09159ed2eeffa97e7500a5347621a8661fb36b69
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-06-29 18:09:21 -0300

    deletando arquivos que não irei usar

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Courses } from './pages/Cources/Courses'
-import { Calendar } from './pages/Calendar'
-import { PrimaryDay } from './pages/Cources/PrimaryDay'
-import { SecondDay } from './pages/Cources/SecondDay'
 
 export function Router() {
     return(
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/courses" element={<Courses/>}/>
-            <Route path="/calendar" element={<Calendar/>}/>
-            <Route path="/primaryday" element={<PrimaryDay/>}/>
-            <Route path="/secondday" element={<SecondDay/>}/>
+
         </Routes>
     )
 }
