3a448d8cf7ed2bf209c094cf138e9832f03c3bd7
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-07-13 17:48:43 -0300

    [KAN-19] [Cources] Criar efeito de carrossel nos cursos

--- src/components/Course/index.tsx
+++ src/components/Course/index.tsx
@@ -4,15 +4,11 @@ import { Container } from "./styles";
 
 export interface CourseProps {
     image: string;
-    topic: string;
-    speaker: string;
-    timeAndDate: string;
-    modality: string;
     url: string;
 }
 
 
-export function Course({image, topic, speaker, timeAndDate, modality, url}:CourseProps) {
+export function Course({image, url}:CourseProps) {
     return(
         <Container >
             <main>
@@ -23,12 +19,6 @@ export function Course({image, topic, speaker, timeAndDate, modality, url}:Cours
                 >
                     <img src={image} alt="Foto do palestrante" />
                     
-                    <div className="description">
-                        <h2>{topic}</h2>
-                        <p><span>Palestrante:</span>  {speaker}</p>
-                        <p><span>Horário: </span> {timeAndDate}</p>
-                        <p><span>Modadlidade: </span>{modality}</p>
-                    </div>
                 </NavLink>
             </main>
         </Container>
